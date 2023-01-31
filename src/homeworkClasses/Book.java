01292cb6cd21088b47cf3d8221f282c59edf7d1d
koss-sh <shilinskii22@gmail.com>
2023-01-31 20:38:51 +0300

    создал геттеры

--- src/homeworkClasses/Book.java
+++ src/homeworkClasses/Book.java
@@ -1,14 +1,26 @@
 package homeworkClasses;
 
 public class Book {
-    String bookName;
-    Author authorName;
-    int publishingYear;
+    private String bookName;
+    private Author authorName;
+    private int publishingYear;
 
     public Book(String bookName, Author authorName, int publishingYear) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public Author getAuthorName() {
+        return this.authorName;
+    }
+
+    public int getPublishingYear() {
+        return this.publishingYear;
     }
 }
 
--- src/homeworkClasses/Main.java
+++ src/homeworkClasses/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Author author1 = new Author("Stephen", "King");
         Author author2 = new Author("George", "Orwell");
-        System.out.println("author1 = " + author1.firstName + " " + author1.lastName);
-        System.out.println("author2 = " + author2.firstName + " " + author2.lastName);
+        System.out.println("author1 = " + author1.getFirstName() + " " + author1.getLastName());
+        System.out.println("author2 = " + author2.getFirstName() + " " + author2.getLastName());
         Book book1 = new Book("It", author1, 1990);
-        System.out.println("book1 = " + book1.bookName + " " + book1.authorName + " " + book1.publishingYear);
+        System.out.println("book1 = " + book1.getBookName() + " " + book1.getAuthorName() + " " + book1.getPublishingYear());
     }
 }
